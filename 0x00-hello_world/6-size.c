78db08c26a8b52740f5f94575efbefcc8b0028d4
kofimuad <iamkofiagyei@gmail.com>
2022-12-08 18:16:39 -0800

    must work now

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,8 +3,8 @@
 /**
  * main - print the size of various types
  *
- * Description: print the size of various types on the computer it is 
- * compiled and run on
+ * Description: prints the size of various types on the computer it is compiled
+ * and run on
  *
  * Return: Always 0 (Success)
  */
