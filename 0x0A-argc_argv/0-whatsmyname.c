9f456ba7864f4784dde3d04d9830f2eb96c78a41
kofimuad <iamkofiagyei@gmail.com>
2023-01-09 02:26:38 -0800

    first commit; program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the name of program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: (0)
+ */
+
+int main(_atrribute_((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
