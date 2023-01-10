018cf9f9bf378f4e1704ba329d33433d0a7c0a62
kofimuad <iamkofiagyei@gmail.com>
2023-01-10 06:01:36 -0800

    cobalt

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -32,7 +32,7 @@ char *str_concat(char *s1, char *s2)
 
 	length3 = length1 + length2;
 
-	arr = (char*) malloc(length3 * sizeof(char) + 1);
+	arr = (char *) malloc(length3 * sizeof(char) + 1);
 
 	if (arr == NULL)
 		return (NULL);
