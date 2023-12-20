b2c9cdf9457c5c653625f8431282b4734e9b7b60
kofimuad <iamkofiagyei@gmail.com>
2023-12-20 04:17:16 +0000

    major edits

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,34 +6,29 @@ void times_table(void)
 {
 	int row;
 	int col;
+	int product;
 
 	for (row = 0; row <= 9; row++)
 	{
-		for (col = 0; col <= 9; col++)
+		_putchar('0');
+		for (col = 1; col <= 9; col++)
 		{
-			int product = row * col;
+			product = row * col;
 
-			/* handling the space requirement*/
-			if (col != 0)
+			_putchar(',');
+			_putchar(' ');
+
+			if (product < 9)
 			{
-				_putchar(',');
 				_putchar(' ');
-				if (product < 10)
-				{
-					_putchar(' ');
-				}
+				_putchar('0' + product);
 			}
-			/* how to handling the output*/
-			if (product >= 10)
+			else
 			{
 				_putchar('0' + product / 10);
 				_putchar('0' + product % 10);
 			}
-			else
-			{
-				_putchar('0' + product);
-			}
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
