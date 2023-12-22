6383fed095b65e9fcbd57aaaa7b2eb512191d4ce
kofimuad <iamkofiagyei@gmail.com>
2023-12-22 01:11:58 +0000

    lets see

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -2,9 +2,9 @@
 
 /**
  * largest_number - returns the largest of 3 numbers
- * @a: first digit
- * @b: second digit
- * @c: third digit
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
  * Return: largest number
  */
 
