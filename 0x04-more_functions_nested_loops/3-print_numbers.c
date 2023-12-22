8e2913d2eb3028bc2ac9f145901ac98bfa97b27b
kofimuad <iamkofiagyei@gmail.com>
2023-12-22 01:49:22 +0000

    remove redundancies

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - function that prints numbers 0 to 9
+ * Return: Nothing
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <=  57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
