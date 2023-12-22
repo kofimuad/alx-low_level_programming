c48ea477d1d87b51f3aa2bea5c18932b36f02633
kofimuad <iamkofiagyei@gmail.com>
2023-12-22 04:23:49 +0000

    whew

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,10 +7,9 @@
 int main(void)
 {
 	int i;
+
 	i = 1;
-
 	printf("%d", i);
-
 	for (i = 2; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
@@ -27,7 +26,7 @@ int main(void)
 		}
 		else
 		{
-			printf(" %d", i);
+			printf("%d", i);
 		}
 	}
 	printf("\n");
