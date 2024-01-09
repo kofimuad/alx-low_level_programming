75d1a5675d536cb7a45e84b6e4e260dabcd0f405
kofimuad <iamkofiagyei@gmail.com>
2024-01-09 23:00:16 +0000

    removed the *

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,14 +9,14 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;
 
-	for (; *s[i] != '\0'; i++)
+	for (; s[i] != '\0'; i++)
 	{
-		if (*s[i] == c)
+		if (s[i] == c)
 		{
 			return (s[i]);
 		}
 	}
-	if (*s[i] == c)
+	if (s[i] == c)
 		return (s[i]);
 	return ('\0');
 }
