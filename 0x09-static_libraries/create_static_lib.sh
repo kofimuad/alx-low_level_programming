22c5190ace7a73696f97813d1f45b9fbd9cf217e
kofimuad <iamkofiagyei@gmail.com>
2024-01-13 02:43:06 +0000

    2 lines

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,24 +1,2 @@
 #!/bin/bash
-
-# Name of the static library
-LIB_NAME="liball.a"
-
-# Creating an empty static library
-ar -rc ${LIB_NAME}
-
-# Compiling each C file in the current directory and adding it to the static library
-for file in *.c; do
-    # Compile the file into an object file
-    gcc -c "${file}"
-    
-    # Extract the filename without extension
-    object_file=$(basename "${file}" .c).o
-
-    # Add the object file to the static library
-    ar -rs ${LIB_NAME} "${object_file}"
-
-    # Optionally remove the object file to clean up
-    rm "${object_file}"
-done
-
-echo "Static library ${LIB_NAME} created successfully."
+gcc -c *.c && ar -rcs liball.a *.o && rm *.o
