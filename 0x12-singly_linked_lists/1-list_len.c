e4737fdecaa19a3e7de1eaa8968793bdc46ede4e
kofimuad <iamkofiagyei@gmail.com>
2024-02-05 21:19:34 +0000

    length of list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "list.h"
+/**
+ * list_len - Determine number of elements in
+ *the linked list list_t
+ *
+ * @h: the start of the linked list
+ *
+ * Return: the number of elements in the linked list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t elements = 0;
+
+	while (h != NULL)
+	{
+		elements++;
+		h = h->next;
+	}
+
+	return (elements);
+}
--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -4,3 +4,6 @@ This is a project on singly linked lists.
 ## Task 0 - Print List
 Write a function that prints the elements of a ```list_t``` list.
 
+## Task 1 - List length
+A function that returns that number of elements in a list_t list.
+
