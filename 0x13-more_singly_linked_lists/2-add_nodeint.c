e7904ecf194d8b25774f10787aed2ac67c54d4c3
kofimuad <iamkofiagyei@gmail.com>
2024-02-06 17:47:34 +0000

    added new node

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * add_nodeint- adds a node to the listint_t list
+ * @head: points to the head of the node
+ * @n: integer stored in the node.
+ *
+ * Return: address of new node
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = *head;
+
+	new_node = *head;
+
+	return (new_node);
+}
--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -7,3 +7,6 @@ Write a function that prints the elements of listint_t list.
 ## Task 1 - List length
 A function that returns the number of elements in a lintint_t list.
 
+## Task 2 - Add node
+A function that adds a node to the beginning of a listint_t list
+
