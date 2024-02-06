9063f8dd8705281d7ed1fc95836dda8f3ba4f594
kofimuad <iamkofiagyei@gmail.com>
2024-02-06 18:48:46 +0000

    node added to end

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - adds node to the end of the listint_t list
+ * @head: beginning of the listint_t list
+ * @n: value to be contained in node created
+ *
+ * Return: Address of new element.
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node;
+	listint-t *temp_node;
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+		*head = new_node;
+	else
+	{
+		temp_node = *head;
+		while (temp_node != NULL)
+			temp_node = temp_node->next;
+		temp_node->next = new_node;
+	}
+	return (new_node);
+}
--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -10,3 +10,6 @@ A function that returns the number of elements in a lintint_t list.
 ## Task 2 - Add node
 A function that adds a node to the beginning of a listint_t list
 
+## Task 3 - Add node at end
+A function that adds a node at the end of a listint_t list.
+
