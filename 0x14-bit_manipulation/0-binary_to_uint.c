dec21f5d39bdf932401559b7f3863007e29ceceb
kofimuad <iamkofiagyei@gmail.com>
2024-02-16 08:14:09 +0000

    bigo changes

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -24,7 +24,7 @@ unsigned int binary_to_uint(const char *b)
 		if (b[len] != '0' && b[len] != '1')
 			return (0);
 
-		num = num + (b[len] - '0') * mult;
+		num +=(b[len] - '0') * mult;
 		mult = mult * 2;
 	}
 
