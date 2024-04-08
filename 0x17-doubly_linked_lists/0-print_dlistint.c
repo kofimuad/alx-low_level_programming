f2401a5c66063c23ec963de29fa8ee2fcb383da9
kofimuad <iamkofiantwi@outlook.com>
2024-04-08 21:48:24 +0000

    solved betty

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,7 +2,7 @@
 /**
  * print_dlistint - prints the number of nodes
  * @h: the start of the node
- * 
+ *
  * Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
@@ -11,7 +11,7 @@ size_t print_dlistint(const dlistint_t *h)
 
 	while (h != NULL)
 	{
-		printf("%d\n",h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
