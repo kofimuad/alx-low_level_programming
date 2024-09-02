cd8b8d021fb960bed24db040c9e3b36df49386a0
kofimuad <iamkofiagyei@gmail.com>
2024-09-02 13:26:22 +0000

    a function that gets the value

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - gets the value in a node for a specific key
+ * @ht - the hash table to search.
+ * @key: the key associated with the value.
+ *
+ * Return: If no value stored NULL
+ *	   Otherwise: value stored.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
+	
+	return ((node == NULL) ? NULL : node->value);
+}
